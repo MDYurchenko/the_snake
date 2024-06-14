c01ba86ac16ff7c6e60a54b3df11fcbd0f39c225
Максим Юрченко <migismigis@gmail.com>
2024-06-14 23:11:07 +0700

    added snake moving fuctionality

--- the_snake.py
+++ the_snake.py
@@ -52,10 +52,12 @@ class Snake(GameObject):
 
     def __init__(self):
         self.length = 1
-        self.positions = () #
+        self.positions = [(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)] #
+        self.position = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         self.direction = (0, 1)
         self.next_direction = None
         self.body_color = (0, 255, 0)
+        self.last = None
 
     def update_direction(self):
      if self.next_direction:
@@ -63,7 +65,27 @@ class Snake(GameObject):
         self.next_direction = None
 
     def move(self):
-        self.positions = [tuple([x + y for x, y in zip(self.positions[0], self.direction)]),] + self.positions[:-1]
+        self.last = self.positions[-1]
+        if self.position[0] + 20*self.direction[0] > SCREEN_WIDTH:
+            y = self.position[1]
+            self.position = (0, y)
+            self.positions[0] = (0, y)
+        elif self.position[0] + 20*self.direction[0] < 0:
+            y = self.position[1]
+            self.position = (SCREEN_WIDTH, y)
+            self.positions[0] = (SCREEN_WIDTH, y)
+        elif self.position[1] + 20*self.direction[1] < 0:
+            x = self.position[0]
+            self.position = (x, SCREEN_HEIGHT)
+            self.positions[0] = (x, SCREEN_HEIGHT)
+        elif self.position[1] + 20 * self.direction[1] > SCREEN_HEIGHT:
+            x = self.position[0]
+            self.position = (x, 0)
+            self.positions[0] = (x, 0)
+        self.positions = [tuple([x + y for x, y in zip(self.positions[0], map(lambda x: x*20, self.direction))]),] + self.positions[:-1]
+        print(self.positions)
+        self.position = self.positions[0]
+
 
     def draw(self):
         for position in self.positions[:-1]:
@@ -86,21 +108,36 @@ class Snake(GameObject):
 
     def reset(self):
         self.length = 1
-        self.positions = [(randint(0, GRID_WIDTH), randint(0, GRID_HEIGHT)), ]
+        self.positions = [(randint(0, SCREEN_WIDTH), randint(0, SCREEN_HEIGHT)), ]
         self.direction = (1, 0)
 
 class Apple(GameObject):
     def __init__(self):
         self.body_color = (255, 0, 0)
-        self.position = (randint(0, GRID_WIDTH), randint(0, GRID_HEIGHT))
+        self.position = (randint(0, SCREEN_WIDTH), randint(0, SCREEN_HEIGHT))
 
     def randomize_position(self):
-        self.position = (randint(0, GRID_WIDTH), randint(0, GRID_HEIGHT))
+        self.position = (randint(0, SCREEN_WIDTH), randint(0, SCREEN_HEIGHT))
 
     def draw(self):
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+def handle_keys(game_object):
+     for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+             pygame.quit()
+             raise SystemExit
+         elif event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_UP and game_object.direction != DOWN:
+                 game_object.next_direction = UP
+             elif event.key == pygame.K_DOWN and game_object.direction != UP:
+                 game_object.next_direction = DOWN
+             elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+                 game_object.next_direction = LEFT
+             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+                 game_object.next_direction = RIGHT
 
 def main():
     # Инициализация PyGame:
@@ -110,11 +147,19 @@ def main():
     snake = Snake()
     apple = Apple()
 
-    # while True:
-    #     clock.tick(SPEED)
+    #pygame.draw.rect(screen, (255, 255, 255), (10, 10, 10, 10))
+    #pygame.display.update()
+
+    while True:
+        clock.tick(SPEED)
 
         # Тут опишите основную логику игры.
-        # ...
+        apple.draw()
+        snake.draw()
+        handle_keys(snake)
+        snake.update_direction()
+        snake.move()
+        pygame.display.update()
 
 
 if __name__ == '__main__':
